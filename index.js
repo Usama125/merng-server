39e8a1ccb326b6f5b5ea11a3a055d3425dd95740
Usama125 <usama.compliance@nxvt.com>
2020-05-31 02:15:17 +0500

    Fixes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MONGODB } = require('./config');
 
 const pubsub = new PubSub();
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
